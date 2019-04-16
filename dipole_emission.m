7ea1334a5a9bf2fe62d998873d0c70ede5b55a7f
jripoll4dnaturecom <jripoll@4dnature.com>
2019-04-16 09:51:00 +0200

    Update dipole_emission.m

--- dipole_emission.m
+++ dipole_emission.m
@@ -12,22 +12,32 @@ k0 = 2*pi*n0/lambda;
 w = k0*c/n0;
 p0 = 1; % normalizing by dipole moment and eps0
 eps0 = 1e-3;
-x = [-2000:5:2000];
-y = [-2000:5:2000];
-%x = [-500:1:500];
-%y = [-500:1:500];
+%x = [-2000:5:2000];
+%y = [-2000:5:2000];
+x = [-lambda/2:0.5:lambda/2];
+y = [-lambda/2:0.5:lambda/2];
 [X,Y] = meshgrid(x,y);
 
 r0 = sqrt(X.^2 + Y.^2);
 theta = unwrap(angle(X + 1i*Y));
 
-fact = logspace(-3,0,10);
+fact = [1 2 5 10];
+showpart = true;
+
+% Prepare the new file.
+folder = 'C:\Users\jripoll\OneDrive - Universidad Carlos III de Madrid\UC3M\Powerpoint\';
+
 
 for ifact = 1:length(fact)
     r = r0*fact(ifact);
+    vidObj = VideoWriter([folder 'dipole_emm' num2str(fact(ifact)) '.avi']);
+    vidObj.FrameRate = 30;
+    open(vidObj);
     
-    L = 100; Rc = 20;
-    t = 2*pi/w*(0:0.05:1);
+    if fact(ifact)>1, showpart = false; end
+    
+    L = 10; Rc = 10;
+    t = 2*pi/w*(0:0.01:4);
     for it=1:length(t)
         coswt = cos(w*t(it) - k0*r);
         sinwt = sin(w*t(it) - k0*r);
@@ -37,18 +47,29 @@ for ifact = 1:length(fact)
         Sr = Ephi.*Hphi;
         Sphi = -Er*Hphi;
         Sr(~isfinite(Sr)) = 0;
-        Sr(Sr>mean(Sr(:))) = mean(Sr(:));
+        Sr(Sr>mea+
+        +n(Sr(:))) = mean(Sr(:));
         Er(~isfinite(Er)) = 0; 
         figure(1)
         hold off
-        imagesc(Er.*r.^2);
-        caxis([-1/fact(ifact) 1/fact(ifact)])
+        imagesc(x*fact(ifact),y*fact(ifact),Er.*r.^2);
+        caxis([-2 2])
+        axis xy
         hold on
-        contour(real(log(Er.*r^2)),'k:')
-        ypos = 395 + cos(w*t(it))*L;
-        circ = circd(Rc,linspace(0,360,90));
-        fill(398+circ(1,:),ypos+circ(2,:),'w');
-        text(398+6-Rc,ypos+6-Rc,'-','FontSize',30)
-        pause(0.1)
+        contour(x*fact(ifact),y*fact(ifact),real(log(Er.*r^2)),'k:')
+        axis off
+        text(max(x)*fact(ifact)*0.3,max(y)*fact(ifact)*0.7,['Scale ' num2str(fact(ifact)) '\lambda'],'FontSize',20,'Color','w')
+            ypos = - sin(w*t(it))*L;
+            circ = circd(Rc*fact(ifact),linspace(0,360,90));
+            fill(circ(1,:),ypos+circ(2,:),'w');
+        if showpart
+            text(-5*fact(ifact),ypos+5*fact(ifact),'-','FontSize',30)
+        end
+        line(lambda/2*[-1 1 1 -1 -1],lambda/2*[-1 -1 1 1 -1],'color','black','LineWidth',0.1,'LineStyle','-')
+        pause(0.001)
+        f = getframe(gcf);
+        writeVideo(vidObj,f);
     end
+% Close the file.
+close(vidObj);
 end
